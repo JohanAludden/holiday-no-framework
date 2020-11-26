283a642b61a31202cd33ea6b62aaf64486f47010
Johan Aludden <johan_aludden@intuit.com>
2020-11-26 12:14:49 +0000

    Added classes for employee

---
+++ app/src/main/java/com/intuit/jaludden/Employee.java
@@ -0,0 +1,4 @@
+package com.intuit.jaludden;
+
+public class Employee {
+}
---
+++ app/src/main/java/com/intuit/jaludden/EmployeeController.java
@@ -0,0 +1,11 @@
+package com.intuit.jaludden;
+
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeController {
+
+    public List<Employee> getEmplyeesFor(String employee) {
+        return Collections.emptyList();
+    }
+}
